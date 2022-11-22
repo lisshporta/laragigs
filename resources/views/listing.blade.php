5f7ffdac7f0f1482c86f364946a849bfc692a3d5
Lis Shporta <lis.shporta@gmail.com>
2022-11-22 17:44:12 +0100

    Project Update

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -4,22 +4,22 @@
 @include('partials._search')
 
 
-<a href="index.html" class="inline-block text-black ml-4 mb-4"
+<a href="/" class="inline-block text-black ml-4 mb-4"
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="p-10">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
         <img
             class="w-48 mr-6 mb-6"
-            src="images/acme.png"
+            src="{{asset('images/no-image.png')}}"
             alt=""
         />
 
-        <h3 class="text-2xl mb-2">Senior Laravel Developer</h3>
-        <div class="text-xl font-bold mb-4">Acme Corp</div>
+        <h3 class="text-2xl mb-2">{{$listing->title}} </h3>
+        <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
         <ul class="flex">
             <li
                 class="bg-black text-white rounded-xl px-3 py-1 mr-2"
@@ -43,7 +43,7 @@
             </li>
         </ul>
         <div class="text-lg my-4">
-            <i class="fa-solid fa-location-dot"></i> Daytona, FL
+            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
         </div>
         <div class="border border-gray-200 w-full mb-6"></div>
         <div>
@@ -51,32 +51,17 @@
                 Job Description
             </h3>
             <div class="text-lg space-y-6">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Eligendi non reprehenderit
-                    facilis architecto autem quam
-                    necessitatibus, odit quod, repellendus
-                    voluptate cum. Necessitatibus a id tenetur.
-                    Error numquam at modi quaerat.
-                </p>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur
-                    adipisicing elit. Quaerat praesentium eos
-                    consequuntur ex voluptatum necessitatibus
-                    odio quos cupiditate iste similique rem in,
-                    voluptates quod maxime animi veritatis illum
-                    quo sapiente.
-                </p>
+               {{$listing->description}}
 
                 <a
-                    href="mailto:test@test.com"
+                    href="mailto:{{$listing->email}}"
                     class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
                     ><i class="fa-solid fa-envelope"></i>
                     Contact Employer</a
                 >
 
                 <a
-                    href="https://test.com"
+                    href="{{$listing->website}}"
                     target="_blank"
                     class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
                     ><i class="fa-solid fa-globe"></i> Visit
@@ -85,7 +70,7 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
 </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/listings/{id}', function($id) {
+Route::get('/listings/{listing}', function(Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });

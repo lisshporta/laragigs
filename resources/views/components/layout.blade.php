8f344a584ac4c4e34fbc4a60294f70da718003ff
Lis Shporta <lis.shporta@gmail.com>
2022-11-24 15:27:53 +0100

    auth Links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,6 +33,19 @@
                 ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+                <li>
+                   <span class="font-bold uppercase">
+                   Welcome {{auth()->user()->name}} 
+                   </span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel"
+                        ><i class="fa-solid fa-gear"></i>
+                        Manage Listings</a
+                    >
+                </li>
+                @else
                 <li>
                     <a href="/register" class="hover:text-laravel"
                         ><i class="fa-solid fa-user-plus"></i> Register</a
@@ -44,6 +57,7 @@
                         Login</a
                     >
                 </li>
+                @endauth
             </ul>
         </nav>
         <main>

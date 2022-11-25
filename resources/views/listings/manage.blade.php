c4ebc9eb993cfd0e2713c4722bc584947f7acd23
Lis Shporta <lis.shporta@gmail.com>
2022-11-25 10:55:05 +0100

    Small Update

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -16,7 +16,7 @@
                     <td
                         class="px-4 py-8 border-t border-b border-gray-300 text-lg"
                     >
-                        <a href="show.html">
+                        <a href="/listings/{{$listing->id}}">
                             {{$listing->title}}
                         </a>
                     </td>
